1a3d04b1d581487e6a7a217f759198720e38794c
root1iu <root1iu@aliyun.com>
2019-01-06 12:07:41 +0800

    add release memory

--- athread.c
+++ athread.c
@@ -6,37 +6,48 @@
 #include <sys/syscall.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <signal.h>
 #include "alist.h"
+#include "sptimer.h"
 
 
 #define STACK_LEN 4096 * 1024
 #define handle_error(msg) do { perror(msg); exit(EXIT_FAILURE);} while(0)
 #define gettid()    syscall(SYS_gettid)
+#define tkill(tid, sig) syscall(SYS_tkill, tid, sig)
+
+#define RELEASE_MEM_TIME 1
+#define ATHREAD_CANCEL 60
 
 #define elem2entry(val_ptr, type, member) (type*)((long int)val_ptr - (long int)(&((type*)0)->member))
 
+typedef void(*SIGNALFN)(int);
 /** #define max_thread 1024 */
 
 bool first_create = true;
+static int thread_num = 1;
+static int timefd;
 
 struct list alive_stack_list;
 struct list wait_rm_stack_list;
 
+//自定义栈结构
 struct stk_list_elem {
-    struct list_elem flag;
-    void*  stk_addr;
-    int    stk_size;
-    pid_t thid;
+    struct list_elem flag;  //加入通用链表的标记
+    void*  stk_addr;        //mmap申请的栈地址
+    int    stk_size;        //mmap申请的栈大小
+    pid_t thid;             //线程号为thid的线程申请的栈
 };
 
-void init_stk_elem(struct stk_list_elem* elem, void* addr, int size, athread_t id) {
+//初始化自定义结构stk_list_elem
+static void init_stk_elem(struct stk_list_elem* elem, void* addr, int size, athread_t id) {
     elem->stk_addr = addr;
     elem->stk_size = size;
     elem->thid = id;
 }
 
 //通过tid来找结点的list回调函数
-bool find_stk_addr_by_tid(struct list_elem* elem, pid_t tid) {
+static bool find_stk_addr_by_tid(struct list_elem* elem, pid_t tid) {
     struct stk_list_elem* temp = elem2entry(elem, struct stk_list_elem, flag);
     if(temp->thid == tid) {
         return true;
@@ -45,28 +56,30 @@ bool find_stk_addr_by_tid(struct list_elem* elem, pid_t tid) {
 }
 
 //通过wait链表释放未释放的mmap映射内存的回调函数
-bool release_mem(struct list_elem* elem, pid_t tid) {
+static bool release_mem(struct list_elem* elem, pid_t tid) {
     struct stk_list_elem* temp = elem2entry(elem, struct stk_list_elem, flag);
     printf("release memory in %p\n", temp->stk_addr);
     munmap(temp->stk_addr, temp->stk_size);
+    printf("after munmap\n");
     printf("malloc memory in %p\n", temp);
     remove_elem(&wait_rm_stack_list, &temp->flag);
     free(temp);
     return false;
 }
 
-void time_action() {
+
+//定时执行内存释放函数
+static void time_action(int arg) {
     list_traversal(&wait_rm_stack_list, release_mem, 0);
+    signal(SIGALRM, time_action);
+    alarm(RELEASE_MEM_TIME);
 }
 
-//static void* stack_array[max_thread];
-//static int thid2stack[max_thread];
-//static int index = 0;
-
+//创建线程
 void athread_create(athread_t* th_id, void* en, int (*fn)(void*), void* arg ) {
     void* start_stack = mmap(NULL, STACK_LEN,
-                            PROT_READ | PROT_WRITE,
-                            MAP_PRIVATE | MAP_ANONYMOUS | MAP_GROWSDOWN, -1, 0);
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANONYMOUS | MAP_GROWSDOWN, -1, 0);
     printf("mmap memory in %p\n", start_stack);
 
     if(start_stack == MAP_FAILED) {
@@ -81,7 +94,7 @@ void athread_create(athread_t* th_id, void* en, int (*fn)(void*), void* arg ) {
             | CLONE_FS      //共享文件系统信息
             | CLONE_FILES   //共享文件描述符表
             | CLONE_IO,     //共享IO上下文
-                (void*) NULL);
+            (void*) NULL);
 
     if(tid == -1) {
         munmap(start_stack, STACK_LEN);
@@ -93,20 +106,37 @@ void athread_create(athread_t* th_id, void* en, int (*fn)(void*), void* arg ) {
     if(first_create) {
         init_list(&alive_stack_list);
         init_list(&wait_rm_stack_list);
+        /** atexit(time_action); */
+        signal(SIGALRM, time_action);
+        alarm(RELEASE_MEM_TIME);
+
+        signal(ATHREAD_CANCEL, (SIGNALFN)athread_exit);
+
+        printf("install time_action\n");
         first_create = false;
     }
 
     //加入alive队列
     struct stk_list_elem* temp = (struct stk_list_elem*)malloc(sizeof(struct stk_list_elem));
+    thread_num++;
     if(temp == NULL) {
         handle_error("malloc");
     }
     printf("malloc memory in %p\n", temp);
     init_stk_elem(temp, start_stack, STACK_LEN, *th_id);
     push_list(&alive_stack_list, &temp->flag);
-    
+
 }
 
+
+//线程取消函数，通过信号来使单个线程执行athread_exit
+void athread_cancel(pid_t tid) 
+{
+    printf("cancel %d\n", tid);
+    tkill(tid, 60);
+}
+
+//线程退出函数
 void athread_exit(void* arg) 
 {
     int stack_index = -1;
@@ -116,6 +146,11 @@ void athread_exit(void* arg)
     printf("pid = %d\n", pid);
 
     if(pid == tid)  {   //main thread do not need munmap
+        thread_num--;
+        if(thread_num == 0) {
+            printf("exit\n");
+            /** exit(0); */
+        }
         printf("main thread exit\n");
         syscall(SYS_exit, 0);
         printf("main thread exit\n");
@@ -123,31 +158,10 @@ void athread_exit(void* arg)
 
     //通过tid找到链表flag
     struct list_elem* temp = list_traversal(&alive_stack_list, find_stk_addr_by_tid, tid);
+    if(temp == NULL) printf("temp == nULL\n");
     remove_elem(&alive_stack_list, temp);    //从alive列表中移除
     /** struct stk_list_elem* stk_elem = elem2entry(temp, struct stk_list_elem, flag); */
     push_list(&wait_rm_stack_list, temp);    //加入删除列表
-    //for(int i = 0; i < max_thread; ++i) {
-    //    if(tid == thid2stack[i]) {
-    //        stack_index = i;
-    //        break;
-    //    }
-    //}
-    //if(stack_index == -1) {  //说明没有这个线程或者create时间片在赋值前断了
-    //    //something wrong
-    //    handle_error("terrible wrong");
 
-    //加入回收队列
-
-    //}
-
-    //release stack memory from mmap
-    //printf("munmap addr %p\n", stack_array[stack_index]);
-    //munmap(stack_array[stack_index], STACK_LEN);
-    //stack_array[stack_index] = NULL;
-    //thid2stack[stack_index] = 0;
-    //printf("tid = %d\n", tid);
-
-    //exit thread;
     syscall(SYS_exit, 0);
-    /** syscall(SYS_exit, *(int*)arg); */
 }
--- athread.h
+++ athread.h
@@ -3,10 +3,16 @@
 
 
 typedef unsigned long int athread_t;
+typedef int pid_t;
 
+//线程退出函数
 void athread_exit(void* arg);
+
+//创建线程
 void athread_create(athread_t* th_id, void* en, int (*fn)(void*), void* arg );
 
-void time_action();
+//线程取消函数，通过信号来使单个线程执行athread_exit
+void athread_cancel(pid_t tid);
+
 
 #endif /* ifndef sPthread */
